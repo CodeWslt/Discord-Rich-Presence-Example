26f30c8e4d0488f9e52b150273225d3d57502843
Wslt <76235453+CodeWslt@users.noreply.github.com>
2021-09-27 23:46:54 -0400

    Add files via upload

---
+++ Discord Rich Presence/Main.Designer.cs
@@ -0,0 +1,208 @@
+﻿
+namespace Discord_Rich_Presence
+{
+    partial class Main
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.DetailsTextbox = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.StateTextbox = new System.Windows.Forms.TextBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label10 = new System.Windows.Forms.Label();
+            this.RPCTextbox = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(9, 159);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(100, 100);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(115, 159);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(29, 15);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "N/A";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(115, 178);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(43, 15);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "RPC.cs";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(115, 198);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(186, 15);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Developing Discord Rich Presence";
+            // 
+            // DetailsTextbox
+            // 
+            this.DetailsTextbox.Location = new System.Drawing.Point(12, 27);
+            this.DetailsTextbox.Name = "DetailsTextbox";
+            this.DetailsTextbox.Size = new System.Drawing.Size(280, 23);
+            this.DetailsTextbox.TabIndex = 5;
+            this.DetailsTextbox.Text = "RPC.cs";
+            this.DetailsTextbox.TextChanged += new System.EventHandler(this.DetailsTextbox_TextChanged);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(9, 9);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(45, 15);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Details:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(9, 55);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(36, 15);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "State:";
+            // 
+            // StateTextbox
+            // 
+            this.StateTextbox.Location = new System.Drawing.Point(12, 73);
+            this.StateTextbox.Name = "StateTextbox";
+            this.StateTextbox.Size = new System.Drawing.Size(280, 23);
+            this.StateTextbox.TabIndex = 7;
+            this.StateTextbox.Text = "Developing Discord Rich Presence";
+            this.StateTextbox.TextChanged += new System.EventHandler(this.StateTextbox_TextChanged);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(301, 56);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(85, 19);
+            this.checkBox1.TabIndex = 17;
+            this.checkBox1.Text = "Timestamp";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(9, 103);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(283, 43);
+            this.button1.TabIndex = 18;
+            this.button1.Text = "Save";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(225)))), ((int)(((byte)(225)))), ((int)(((byte)(225)))));
+            this.panel1.Location = new System.Drawing.Point(9, 152);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(572, 1);
+            this.panel1.TabIndex = 19;
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(298, 9);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(54, 15);
+            this.label10.TabIndex = 21;
+            this.label10.Text = "RPC Key:";
+            // 
+            // RPCTextbox
+            // 
+            this.RPCTextbox.Location = new System.Drawing.Point(301, 27);
+            this.RPCTextbox.Name = "RPCTextbox";
+            this.RPCTextbox.Size = new System.Drawing.Size(280, 23);
+            this.RPCTextbox.TabIndex = 20;
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(598, 275);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.RPCTextbox);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.StateTextbox);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.DetailsTextbox);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Main";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox DetailsTextbox;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox StateTextbox;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.TextBox RPCTextbox;
+    }
+}
+
---
+++ Discord Rich Presence/Main.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Drawing;
+using System.Reflection;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Discord_Rich_Presence
+{
+    public partial class Main : Form
+    {
+        public Main()
+        {
+            InitializeComponent();
+            this.Text = $"Discord Rich Presence By Wslt | Version 1.0";
+            this.Icon = Icon.ExtractAssociatedIcon(Assembly.GetExecutingAssembly().Location);
+            RPCTextbox.Text = Properties.Settings.Default.RPCID;
+            DetailsTextbox.Text = Properties.Settings.Default.Details;
+            StateTextbox.Text = Properties.Settings.Default.State;
+            if (Properties.Settings.Default.TimeStamp == true)
+                checkBox1.Checked = true;
+        }
+        private void DetailsTextbox_TextChanged(object sender, EventArgs e) => label2.Text = DetailsTextbox.Text;
+        private void StateTextbox_TextChanged(object sender, EventArgs e) => label3.Text = StateTextbox.Text;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            RPC.SetRPC(DetailsTextbox.Text, StateTextbox.Text, checkBox1.Checked);
+            Task.Run(() => {
+                CheckForIllegalCrossThreadCalls = false;
+                try
+                {
+                    pictureBox1.ImageLocation = RPC.UserPfp;
+                    label1.Text = RPC.Username;
+                }
+                catch { pictureBox1.LoadAsync("https://www.galaxyswapperv2.com/Icons/InvalidIcon.png"); }
+            });
+            Properties.Settings.Default.RPCID = RPCTextbox.Text;
+            Properties.Settings.Default.Details = DetailsTextbox.Text;
+            Properties.Settings.Default.State = StateTextbox.Text;
+            Properties.Settings.Default.TimeStamp = checkBox1.Checked;
+            Properties.Settings.Default.Save();
+        }
+    }
+}
